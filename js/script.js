b1c591051952c8f4a3940d7a0aa7c0512928556a
DomanicLeo <naylinhtet2712001@gmail.com>
2023-03-31 22:09:45 +0630

    Responsive Omnifood Final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Mobile Navigation
 document.querySelector(".btn-mobile-nav").addEventListener("click",function(){
     document.querySelector(".header").classList.toggle("nav-open");
 });
@@ -23,3 +24,41 @@ allLink.forEach(function(link){
     });
 }); 
 
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function(entries){
+    const ent = entries[0];
+    console.log(ent)
+    if(ent.isIntersecting === false){
+        document.body.classList.add("sticky");
+    }
+    if(ent.isIntersecting === true){
+        document.body.classList.remove("sticky");
+    }
+},{
+    // In The Veiw Port
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+});
+
+obs.observe(sectionHeroEl);
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+  
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+  
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+  
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  }
+  checkFlexGap();
